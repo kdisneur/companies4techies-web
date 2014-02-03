7b168c6aa0dddf9983859b8890bbd1b36b931c36
Kevin <kevin@koboyz.org>
2014-02-03 17:21:59 +0100

    Add kibana

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,13 @@ VERSION=${VERSION:-0.90.10}
 app_name="elasticsearch-${VERSION}"
 tgz_name="${app_name}.tar.gz"
 
-sudo apt-get install -y openjdk-7-jre
+sudo apt-get install -y openjdk-7-jre zip
+wget http://download.elasticsearch.org/kibana/kibana/kibana-latest.zip
+unzip kibana-latest.zip
+mv kibana-latest kibana
+mv kibana elasticsearch/.
+rm kibana-latest.zip
+ln -s elasticsearch/kibana-companies-dashboards.json elasticsearch/kibana/app/dashboards/companies.json
 wget "https://download.elasticsearch.org/elasticsearch/elasticsearch/${tgz_name}"
 tar zxf ${tgz_name}
 mv ${app_name} elasticsearch/elasticsearch
